0e0dde72e3885d3e1142ccc82ad9fb2b923eae69
MCProtein <chss3339@gmail.com>
2025-08-04 23:25:35 +0900

    tf 설정, codepipeline용 assume role 생성

---
+++ iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "codepipeline" {
+  name               = "${local.prefix}-codepipeline"
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume.json
+}
+
+data "aws_iam_policy_document" "codepipeline_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
+  role       = aws_iam_role.codepipeline.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
+}
